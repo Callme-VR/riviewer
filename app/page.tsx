2f23f225b91da0a0b1b84d1f370a6d91fdf5526f
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2025-12-11 18:36:43 +0530

    made it

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function HomePage(){
+  return(
+    <div className="text-center text-4xl">
+      <Button>vishal</Button>
+    </div>
+  )
+}
